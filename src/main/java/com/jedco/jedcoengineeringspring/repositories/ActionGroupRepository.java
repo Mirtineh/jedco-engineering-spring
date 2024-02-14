86f59323a7e020f68114e9a8ca6bc31a05b687b7
Mirtineh <mirfil92@gmail.com>
2024-02-14 14:01:38 +0200

    implement repositories

---
+++ src/main/java/com/jedco/jedcoengineeringspring/repositories/ActionGroupRepository.java
@@ -0,0 +1,7 @@
+package com.jedco.jedcoengineeringspring.repositories;
+
+import com.jedco.jedcoengineeringspring.models.ActionGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActionGroupRepository extends JpaRepository<ActionGroup,Long> {
+}
---
+++ src/main/java/com/jedco/jedcoengineeringspring/repositories/DispatchedMeterRepository.java
@@ -0,0 +1,7 @@
+package com.jedco.jedcoengineeringspring.repositories;
+
+import com.jedco.jedcoengineeringspring.models.DispatchedMeter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DispatchedMeterRepository extends JpaRepository<DispatchedMeter,String> {
+}
