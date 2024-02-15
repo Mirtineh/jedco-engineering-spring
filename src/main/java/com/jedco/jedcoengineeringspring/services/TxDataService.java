1581a5075731635fa3cd867d04d32d56991c2640
Mirtineh <mirfil92@gmail.com>
2024-02-15 08:46:20 +0200

    add service interfaces

---
+++ src/main/java/com/jedco/jedcoengineeringspring/services/TxDataService.java
@@ -0,0 +1,32 @@
+package com.jedco.jedcoengineeringspring.services;
+
+import com.jedco.jedcoengineeringspring.rest.request.TxReadingRequest;
+import com.jedco.jedcoengineeringspring.rest.response.CreateBoxNumberResponse;
+import com.jedco.jedcoengineeringspring.rest.response.PoleResponse;
+import com.jedco.jedcoengineeringspring.rest.response.TransformerResponse;
+import com.jedco.jedcoengineeringspring.rest.response.TxResponse;
+
+import java.security.Principal;
+import java.util.List;
+
+public interface TxDataService {
+    List<String> listFeeder();
+
+    List<TxResponse> listTxByFeeder(String feeder);
+
+    List<PoleResponse> listPoleByFeederTx(String feeder, String tx);
+
+    List<TransformerResponse> listTransformerByFeeder(String feeder);
+
+    List<TransformerResponse> listTransformer();
+
+    List<TransformerResponse> listTransformerByFeederAndTx(String feeder, String txCode);
+
+    List<TransformerResponse> listTransformerByDate(String date);
+
+    void addTxReadingWithLineReadings(Long txReadingId, TxReadingRequest txReadingDTO, Principal principal);
+
+    CreateBoxNumberResponse createBoxNumber(Long poleId, Principal principal);
+
+
+}
---
+++ src/main/java/com/jedco/jedcoengineeringspring/services/TxDataServiceImpl.java
@@ -0,0 +1,63 @@
+package com.jedco.jedcoengineeringspring.services;
+
+import com.jedco.jedcoengineeringspring.rest.request.TxReadingRequest;
+import com.jedco.jedcoengineeringspring.rest.response.CreateBoxNumberResponse;
+import com.jedco.jedcoengineeringspring.rest.response.PoleResponse;
+import com.jedco.jedcoengineeringspring.rest.response.TransformerResponse;
+import com.jedco.jedcoengineeringspring.rest.response.TxResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.security.Principal;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TxDataServiceImpl implements TxDataService {
+    @Override
+    public List<String> listFeeder() {
+        return null;
+    }
+
+    @Override
+    public List<TxResponse> listTxByFeeder(String feeder) {
+        return null;
+    }
+
+    @Override
+    public List<PoleResponse> listPoleByFeederTx(String feeder, String tx) {
+        return null;
+    }
+
+    @Override
+    public List<TransformerResponse> listTransformerByFeeder(String feeder) {
+        return null;
+    }
+
+    @Override
+    public List<TransformerResponse> listTransformer() {
+        return null;
+    }
+
+    @Override
+    public List<TransformerResponse> listTransformerByFeederAndTx(String feeder, String txCode) {
+        return null;
+    }
+
+    @Override
+    public List<TransformerResponse> listTransformerByDate(String date) {
+        return null;
+    }
+
+    @Override
+    public void addTxReadingWithLineReadings(Long txReadingId, TxReadingRequest txReadingDTO, Principal principal) {
+
+    }
+
+    @Override
+    public CreateBoxNumberResponse createBoxNumber(Long poleId, Principal principal) {
+        return null;
+    }
+}
