9b2c50c0f72eda2bb1477c916c6dac947bdcbd14
Mirtineh <mirfil92@gmail.com>
2024-02-16 10:55:04 +0200

    implement commissiong service

--- src/main/java/com/jedco/jedcoengineeringspring/repositories/DispatchedMeterRepository.java
+++ src/main/java/com/jedco/jedcoengineeringspring/repositories/DispatchedMeterRepository.java
@@ -3,5 +3,8 @@ package com.jedco.jedcoengineeringspring.repositories;
 import com.jedco.jedcoengineeringspring.models.DispatchedMeter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DispatchedMeterRepository extends JpaRepository<DispatchedMeter,String> {
+    List<DispatchedMeter> findByMeterNo(String meterNo);
 }
