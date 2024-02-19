2b6324051f149b712644c1ccfdd6585fd05b5acc
Mirtineh <mirfil92@gmail.com>
2024-02-19 09:45:28 +0200

    implement tx user action and user role services

--- src/main/java/com/jedco/jedcoengineeringspring/mappers/UserMapper.java
+++ src/main/java/com/jedco/jedcoengineeringspring/mappers/UserMapper.java
@@ -1,10 +1,13 @@
 package com.jedco.jedcoengineeringspring.mappers;
 
+import com.jedco.jedcoengineeringspring.models.ActionGroup;
 import com.jedco.jedcoengineeringspring.models.RoleDefinition;
 import com.jedco.jedcoengineeringspring.models.UserAction;
 import com.jedco.jedcoengineeringspring.models.UserRole;
 import com.jedco.jedcoengineeringspring.rest.response.RoleDefinitionResponse;
 import com.jedco.jedcoengineeringspring.rest.response.UserActionResponse;
+import com.jedco.jedcoengineeringspring.rest.response.UserActionsListByGroupResponse;
+import com.jedco.jedcoengineeringspring.rest.response.UserRoleResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
@@ -29,4 +32,15 @@ public interface UserMapper {
     default List<UserActionResponse> mapUserActionList(UserAction userAction){
         return Collections.singletonList(toUserActionResponse(userAction));
     }
+
+    @Mapping(target = "groupName", source = "name")
+    @Mapping(target = "groupId", source = "id")
+    @Mapping(target = "groupDescription", source = "description")
+    @Mapping(target = "actionsDtos", source = "userActions")
+    UserActionsListByGroupResponse toUserAction(ActionGroup actionGroup);
+
+    @Mapping(target = "name", source = "roleName")
+    @Mapping(target = "description", source = "roleDescription")
+    @Mapping(target = "createdOn", source = "registeredDate")
+    UserRoleResponse toUserRoleResponse(UserRole userRole);
 }
